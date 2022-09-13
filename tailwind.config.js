b653e88fc6b1440661f15f2a4ad1d6dcf73b5ef0
Karthik Vijayasarathi <karthikkudligi30@gmail.com>
2022-09-13 11:48:34 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*"],
+
+  theme: {
+    colors: {
+      "sky-blue": "#00BAF2",
+      "custom-white": "#FFFFFF",
+      "dark-blue": "#002970",
+      "sky-grey": "#F3F4F6",
+      "black-but": "#000000",
+      "light-grey": "#F5F7FA",
+      paytm: "#00baf2",
+      paytmhover: "#002970",
+      card: "#66d6ff",
+    },
+
+    extend: {
+      backgroundImage: {},
+    },
+  },
+
+  plugins: [],
+};
